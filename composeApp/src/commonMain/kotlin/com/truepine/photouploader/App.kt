08fd510e2ccd38196d9cc977d4a70a7d0ff74b69
marcel <marcel.van.heerwaarden@gmail.com>
2025-11-04 16:49:12 +0100

    Added icon.

--- composeApp/src/commonMain/kotlin/com/truepine/photouploader/App.kt
+++ composeApp/src/commonMain/kotlin/com/truepine/photouploader/App.kt
@@ -16,8 +16,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import photouploader.composeapp.generated.resources.Res
-import photouploader.composeapp.generated.resources.compose_multiplatform
+import com.truepine.photouploader.resources.Res
+import com.truepine.photouploader.resources.compose_multiplatform
 
 @Composable
 @Preview
